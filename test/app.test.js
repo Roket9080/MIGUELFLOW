efa668afd0ec85d2bddcb819fd8e10f6b3f6e216
Miguelito90 <migueherazo2004@gmail.com>
2026-04-14 11:35:36 -0500

    se generaron algunos cambios

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,4 @@
-import { saludar, despedir, estadoSistema, sumar, restar, healthCheck } from "../src/app.js"
+import { saludar, despedir, estadoSistema,sumar,healthCheck } from "../src/app.js"
 
 function ejecutarPruebas() {
     let pasadas = 0;
@@ -37,28 +37,13 @@ function ejecutarPruebas() {
         console.log("Test 3 Fallido:", sumarResultado);
         fallidas++;
     }
-
-
-    const c = 50;
-    const d = 43;
-
-    const restarResultado = restar(c, d);
-
-    console.log("Resultado de restar:", c, "-", d, "=", restarResultado);
-    if (restarResultado === c - d) {
-        console.log("Test 4 pasado: funcion restar funcion correctamente");
-        pasadas++;
-    } else {
-        console.log("Test 4 Fallido:", restarResultado);
-        fallidas++;
-    }
-
+    
     const health = healthCheck();
     if (health.status === 'ok') {
-        console.log('Test healthCheck pasado');
+        console.log('Test 5 healthCheck pasado');
         pasadas++;
     } else {
-        console.log('Test healthCheck fallido', health);
+        console.log('Test 5 healthCheck fallido', health);
         fallidas++;
     }
 
